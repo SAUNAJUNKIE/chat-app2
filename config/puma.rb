ba7f245d0125f2348237b92ff9ab696667413d73
SAUNAJUNKIE <hijiri0420@gmail.com>
2023-02-12 22:27:42 +0900

    hennkou

--- config/puma.rb
+++ config/puma.rb
@@ -12,7 +12,7 @@ threads min_threads_count, max_threads_count
 #
 port        ENV.fetch("PORT") { 3000 }
 
-# Specifies the `environment` that Puma will run in.
+# ifies the `environment` that Puma will run in.
 #
 environment ENV.fetch("RAILS_ENV") { "development" }
 
@@ -27,7 +27,7 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 #
 # workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 
-# Use the `preload_app!` method when specifying a `workers` number.
+# Use the `preload_app!` method when ifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
 # before forking the application. This takes advantage of Copy On Write
 # process behavior so workers use less memory.
